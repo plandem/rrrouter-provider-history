6f74d6b898e5426fd9f9d71ce717662d96779768
plandem <plandem@gmail.com>
2017-04-11 14:22:43 +0300

    fixes for upcoming React.PropTypes changes

--- src/provider.js
+++ src/provider.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import pick from 'lodash/pick';
 import { Provider, RouterProps } from 'rrrouter';
 import { parseHref } from 'rrrouter-history';
